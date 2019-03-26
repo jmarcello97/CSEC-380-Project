55b73a6b3e9fc533b80a86ff339cd2d3fd210ffc
jasonmarcello <jkm7264@g.rit.edu>
2019-03-26 17:01:16 -0400

    changed to post request

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -2,7 +2,7 @@ import pytest
 import requests
 
 def auth_attempt():
-	return requests.get("http://localhost:5000", {"username": "username", "password": "password"}.text)	
+	return requests.post("http://localhost:5000", {"username": "username", "password": "password"}.text)	
 
 def test_authentication():
 	assert "youtube" in auth_attempt()
