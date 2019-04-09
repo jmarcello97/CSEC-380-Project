18955e3f9ae07979b783c443c5c50e6f31ea943e
jasonmarcello <jkm7264@g.rit.edu>
2019-04-09 18:51:43 -0400

    travis test logs in, views video, and deletes video

--- tests/test_upload.py
+++ tests/test_upload.py
@@ -6,9 +6,11 @@ def upload_attempt():
 	auth = s.post("http://localhost:5000", {"username": "admin", "password": "password"})
 	upload = s.post("http://localhost:5000/upload", files={"file": open('ocean.mp4', 'rb')}).text
 	upload_success = "ocean.mp4" in upload
+	get_video = s.get("http://localhost:5000/static/videos/ocean.mp4").status_code
+	get_video_success = get_video == 200
 	delete = s.get("http://localhost:5000/delete_video/ocean.mp4").text
 	delete_success = "ocean.mp4" not in delete
-	return upload_success and delete_success
+	return upload_success and delete_success and get_video_success
 
 def test_authentication():
-	assert "ocean.mp4" in upload_attempt()
+	assert upload_attempt()
